0f7c97d97f77aff91b86429fa0931085c09a6f30
timcrepinpro <tim.crepin@edouardgand.fr>
2026-03-26 13:15:39 +0100

    try/catch

--- Program.cs
+++ Program.cs
@@ -4,34 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Livre livre1 = new Livre (1,"jcp",true,"mathieu merrio",89);
-        livre1.Afficher();
-        livre1.ToString();
-        DVD dvd1 = new DVD (2,"jcp",true,"mathieu merrio",89);
-        dvd1.Afficher();
-        dvd1.ToString();
+        try
+        {
+            Console.WriteLine("Hello, World!");
+            Livre livre1 = new Livre (1,"jcp",true,"mathieu merrio",89);
+            livre1.Afficher();
+            livre1.ToString();
+            DVD dvd1 = new DVD (2,"jcp",true,"mathieu merrio",89);
+            dvd1.Afficher();
+            dvd1.ToString();
 
-        List<Document> documents = new List<Document>
-        {
-            new Livre (1,"jcp",true,"mathieu merrio",89),
-            new DVD (2,"auqune idée",true,"jonathan zablot",89)
-        };
-        foreach (Document document in documents)
-        {
-            if(document is Livre livre)
+            List<Document> documents = new List<Document>
             {
-                livre.Afficher();
-            }
-            if(document is DVD dvd)
+                new Livre (1,"jcp",true,"mathieu merrio",89),
+                new DVD (2,"auqune idée",true,"jonathan zablot",89)
+            };
+            foreach (Document document in documents)
             {
-                dvd.Afficher();
-            }
-            else
-            {
-                document.Afficher();
+                if(document is Livre livre)
+                {
+                    livre.Afficher();
+                }
+                if(document is DVD dvd)
+                {
+                    dvd.Afficher();
+                }
+                else
+                {
+                    document.Afficher();
+                }
             }
         }
+        catch(Exception e)
+        {
+            Console.WriteLine(e.Message);
+            
+        }
+        
 
         try
         {
@@ -56,6 +65,7 @@ class Program
             Console.WriteLine(e.Message);
             
         }
+        
 
         Dictionary<int , string> empreint = new Dictionary<int , string>();
         empreint.Add(1,"mathieu merrio");
